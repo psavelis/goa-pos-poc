abb8922ae6652c03bf1fdbfaf09b558fea3ca502
pedro <savelis.pedro@gmail.com>
2017-11-05 19:36:39 -0200

    add goa dsl api setup

---
+++ design/design.go
@@ -0,0 +1,58 @@
+package design
+
+import . "github.com/goadesign/goa/design"
+import . "github.com/goadesign/goa/design/apidsl"
+
+var _ = API("pos", func() {
+	Description("point of sale microservice")
+	Host("localhost:5001")
+})
+
+var PurchasePayload = Type("Purchase", func() {
+	Description("Detailed information regarding a POS purchase operation")
+
+	Attribute("Locator", String, "Operation reference code", func() {
+		MinLength(1)
+		MaxLength(30)
+	})
+
+	Attribute("PurchaseValue", Number, "Total amount paid", func() {
+		Minimum(0.01)
+	})
+
+	Required("Locator", "PurchaseValue")
+})
+
+var PurchaseMedia = MediaType("application/psavelis.pos.purchase", func() {
+	TypeName("Purchase")
+	Reference(PurchasePayload)
+
+	Attributes(func() {
+		Attribute("TransactionId", String, "Unique transaction identifier")
+		Attribute("Locator")
+		Attribute("PurchaseValue")
+		Required("TransactionId", "Locator", "PurchaseValue")
+	})
+})
+
+var _ = Resource("Purchase", func() {
+	Description("A pos purchase data")
+	BasePath("/purchases")
+
+	Action("create", func() {
+		Description("creates a purchase")
+		Routing(POST("/"))
+		Payload(PurchasePayload)
+		Response(Created)
+	})
+
+	Action("find", func() {
+		Description("retrieve an specific purchase")
+		Routing(GET("/:TransactionId"))
+		Payload(PurchasePayload)
+		Params(func() {
+			Param("TransactionId", String)
+		})
+		Response(OK, PurchaseMedia)
+	})
+})
