23f9fead02aa3ba5576b2e1f24e722755c149026
pedro <savelis.pedro@gmail.com>
2017-11-09 00:04:22 -0200

    add mgo session

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/middleware"
 	"github.com/psavelis/goa-pos-poc/app"
+	mgo "gopkg.in/mgo.v2"
+    bson "gopkg.in/mgo.v2/bson"
 )
+
+type DataStore struct {
+	session  *mgo.Session
+}
 
 func main() {
 	// Create service
@@ -17,6 +23,17 @@ func main() {
 	service.Use(middleware.LogRequest(true))
 	service.Use(middleware.ErrorHandler(service, true))
 	service.Use(middleware.Recover())
+
+	// MongoDB connection pool setup
+	session, err := mgo.Dial("localhost")
+	if(err != nil){
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic)
+
+	DataStore.session = session
 
 	// Mount "Purchase" controller
 	c := NewPurchaseController(service)
@@ -29,5 +46,5 @@ func main() {
 	if err := service.ListenAndServe(":5001"); err != nil {
 		service.LogError("startup", "err", err)
 	}
-
+,
 }
