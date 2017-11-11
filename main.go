e2e3f06eaca1f4327ff74e77543dca8e02a0cb67
pedro <savelis.pedro@gmail.com>
2017-11-11 20:50:49 -0200

    port

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ func main() {
 	app.MountSwaggerController(service, cs)
 
 	// Start service
-	if err := service.ListenAndServe(":80"); err != nil {
+	if err := service.ListenAndServe(":5000"); err != nil {
 		service.LogError("startup", "err", err)
 	}
 }
